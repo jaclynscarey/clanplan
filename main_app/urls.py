c0617a0c255d3e28bbe51f7a0bea32ffc61370f4
Jaclyn Carey <jsoyama@gmail.com>
2023-06-07 17:39:18 -0400

    wip: implement event detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('events/', views.events_index, name='index'),
+    path('events/<int:event_id>/', views.events_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,10 @@ def events_index(request):
     return render(request, 'events/index.html', {
         'events': events
         })
+
+def events_detail(request, event_id):
+    event = Event.objects.get(id=event_id)
+    return render(request, 'events/detail.html', {
+        'event': event
+        })
+
