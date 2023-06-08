4df1012fb15f3507bba2124422054ced1f043766
Jaclyn Carey <jsoyama@gmail.com>
2023-06-08 13:38:07 -0400

    wip: add login and logout redirect

--- ClanPlan/settings.py
+++ ClanPlan/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+LOGIN_REDIRECT_URL = '/events/'
+LOGOUT_REDIRECT_URL = '/'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
