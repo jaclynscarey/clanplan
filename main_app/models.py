d9751c6ec178494606da0d949cdcc45657c4a4c6
Jaclyn Carey <jsoyama@gmail.com>
2023-06-08 13:22:13 -0400

    wip: create relationship between event and user

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Event(models.Model):
@@ -8,6 +10,7 @@ class Event(models.Model):
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
