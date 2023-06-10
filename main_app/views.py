08ed47cf951188af07c9d14db91e75266140d24f
Jaclyn Carey <jsoyama@gmail.com>
2023-06-10 00:42:33 -0400

    wip: add login required

--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,40 @@
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from .models import Event
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def events_index(request):
     events = Event.objects.all()
     return render(request, 'events/index.html', {
         'events': events
         })
 
+@login_required
 def events_detail(request, event_id):
     event = Event.objects.get(id=event_id)
     return render(request, 'events/detail.html', {
         'event': event
         })
 
-class EventCreate(CreateView):
+class EventCreate(LoginRequiredMixin, CreateView):
     model = Event
-    fields = '__all__'
+    fields = ['title', 'description', 'date', 'time', 'location', 'attendees']
 
-class EventUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)    
+
+class EventUpdate(LoginRequiredMixin, UpdateView):
     model = Event
-    fields = '__all__'
+    fields = ['title', 'description', 'date', 'time', 'location', 'attendees']
 
-class EventDelete(DeleteView):
+class EventDelete(LoginRequiredMixin, DeleteView):
     model = Event
     fields = '__all__'
     success_url = '/events/'
