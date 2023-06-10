ab99c026f351d61f59cc6eac5611911717ca11b4
Jaclyn Carey <jsoyama@gmail.com>
2023-06-10 00:40:11 -0400

    wip: define Event, Attendee, UserProfile, and Family models

--- main_app/models.py
+++ main_app/models.py
@@ -1,27 +1,50 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Family(models.Model):
+    name = models.CharField(max_length=100)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin_of_families') # Admin user of the family
+    secret_code = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.name
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(null=True, blank=True)
+    families = models.ManyToManyField(Family, related_name='memberships', blank=True) # User's families
+    
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    def save(self, *args, **kwargs):
+        is_new = not self.pk  # Check if the instance is being created
+        super().save(*args, **kwargs)
+        if is_new:
+            # Create an Attendee instance when a UserProfile is added to an Event
+            for event in self.event_set.all():
+                Attendee.objects.create(user=self, event=event)
+    
 class Event(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=250)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # User who created the event
+    attendees = models.ManyToManyField(UserProfile)
+    
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'event_id': self.id})
     
-class CustomUser(AbstractUser):
-    birthdate = models.DateField(null=True)
+class Attendee(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     
     def __str__(self):
-        return self.username
+        return f'{self.user} is attending {self.event}'
