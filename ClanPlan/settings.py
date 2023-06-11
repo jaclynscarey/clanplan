d702c2500197f66cfa36242d8dbb63f1add32b9c
Jaclyn Carey <jsoyama@gmail.com>
2023-06-10 23:53:26 -0400

    wip: replace db

--- ClanPlan/settings.py
+++ ClanPlan/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ClanPlan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'clanplan',
+        'NAME': 'clan-plan',
     }
 }
 
